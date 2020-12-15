e0a4a67b66b35fed38a2c053c7b5b02000a6f08a
Md. Akhtur Uz Zaman <akhturtanvir@gmail.com>
2020-12-15 20:38:00 +0600

    Update ProductController & apiResource

---
+++ app/Http/Controllers/API/V1/ProductController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Model\Product AS pm;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(pm::get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $response = pm::create($input);
+
+        return response()->json($response);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(pm::find($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+        
+        $data = pm::find($id);
+        $response = $data->update($input);
+
+        return response()->json($response);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return response()->json(pm::find($id)->delete());
+    }
+}
