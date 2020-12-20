d724273599408338eac5f435333bea50cbc2829d
Md. Akhtur Uz Zaman <akhturtanvir@gmail.com>
2020-12-20 16:41:52 +0600

    Work throuth  Routing and Controller && tymon/jw-autht

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,25 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function(){
-    
+Route::middleware('cors')->prefix('v1')->group(function(){
+
     Route::apiResources([
         'products' => 'API\V1\ProductController'
     ]);
+    
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
